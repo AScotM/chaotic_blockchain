2b3108724738f12cfbee5121025a947a15852b69
Arnoldas Skinderis <102791583+AScotM@users.noreply.github.com>
2025-11-06 10:39:33 +0200

    Update chaotic_bloackchain.pl

--- chaotic_bloackchain.pl
+++ chaotic_bloackchain.pl
@@ -6,13 +6,10 @@ use Digest::SHA qw(sha256_hex);
 use Time::HiRes qw(usleep);
 use POSIX qw(strftime);
 
-# --- Transaction Base Data ---
 my @actions = ("DEPOSIT", "WITHDRAW", "TRANSFER", "EXCHANGE", "LOAN", "PAYMENT");
 my @currencies = ("USD", "EUR", "BTC", "ETH", "XMR", "ZEC");
-my @accounts = map { "ACC" . int(rand(99999)) } (1..10);
 my @amounts = map { $_ * 10 } (1..100);
 
-# --- Markov Chain for Transaction Types ---
 my %markov_chain = (
     "DEPOSIT"  => ["WITHDRAW", "TRANSFER", "EXCHANGE"],
     "WITHDRAW" => ["LOAN", "PAYMENT", "DEPOSIT"],
@@ -22,18 +19,20 @@ my %markov_chain = (
     "PAYMENT"  => ["EXCHANGE", "TRANSFER", "LOAN"],
 );
 
-# --- Hidden Rules (Fraud Detection, Self-Corrections) ---
 my %hidden_rules = (
-    "ACC12345" => sub { return "BLOCKED"; },
-    "ACC67890" => sub { return (rand() > 0.8) ? "REVERSED" : ""; },
-    "XMR"      => sub { return (rand() > 0.7) ? "SUSPICIOUS" : ""; },
+    "ACC12345" => sub { return "BLOCKED" },
+    "ACC67890" => sub { return (rand() > 0.8) ? "REVERSED" : "" },
+    "XMR"      => sub { return (rand() > 0.7) ? "SUSPICIOUS" : "" },
 );
 
-# --- Blockchain Data ---
 my @blockchain;
-my $previous_hash = "GENESIS";  # First block has no previous hash
+my $previous_hash = "GENESIS";
+my $max_blocks = 50;
 
-# --- Chaotic Proof-of-Work Function ---
+sub generate_account {
+    return "ACC" . sprintf("%05d", int(rand(99999)));
+}
+
 sub mine_block {
     my ($data, $difficulty) = @_;
     my $nonce = 0;
@@ -45,22 +44,24 @@ sub mine_block {
     return ($hash, $nonce);
 }
 
-# --- Infinite Blockchain Generation ---
 my $current_action = (shuffle @actions)[0];
+my $block_count = 0;
 
-while (1) {
-    # Choose next transaction type
+while ($block_count < $max_blocks) {
     my $next_actions = $markov_chain{$current_action};
     $current_action = (shuffle @$next_actions)[0];
 
-    # Generate transaction details
-    my $account_from = (shuffle @accounts)[0];
-    my $account_to = (shuffle @accounts)[1];
+    my $account_from = generate_account();
+    my $account_to = generate_account();
+    
+    while ($account_from eq $account_to) {
+        $account_to = generate_account();
+    }
+    
     my $currency = (shuffle @currencies)[0];
     my $amount = (shuffle @amounts)[0];
     my $timestamp = strftime("%Y-%m-%d %H:%M:%S", localtime);
 
-    # Check hidden rules
     my $rule_result = "";
     $rule_result = $hidden_rules{$account_from}->() if exists $hidden_rules{$account_from};
     $rule_result = $hidden_rules{$currency}->() if exists $hidden_rules{$currency};
@@ -75,18 +76,16 @@ while (1) {
             my $last_block = pop @blockchain;
             print "[RULE] REVERSING BLOCK: $last_block->{hash}\n";
             $previous_hash = $last_block->{previous_hash};
+            $block_count--;
         }
         next;
     }
 
-    # Create transaction data
     my $transaction = "$timestamp | $current_action | $account_from -> $account_to | $amount $currency | PrevHash: $previous_hash";
 
-    # Proof-of-Work (difficulty = 4)
     print "Mining block...\n";
     my ($block_hash, $nonce) = mine_block($transaction, 4);
 
-    # Store block
     my %block = (
         timestamp => $timestamp,
         transaction => $transaction,
@@ -97,16 +96,17 @@ while (1) {
 
     push @blockchain, \%block;
     $previous_hash = $block_hash;
+    $block_count++;
 
-    # Output block data
     print "BLOCK MINED:\n";
     print "Timestamp  : $block{timestamp}\n";
     print "Transaction: $block{transaction}\n";
     print "Nonce      : $block{nonce}\n";
     print "Hash       : $block{hash}\n";
+    print "Block Count: $block_count/$max_blocks\n";
     print "--------------------------------------\n";
 
-    # Random delay
     usleep(int(rand(800000) + 200000));
 }
 
+print "Blockchain simulation completed with $block_count blocks.\n";
